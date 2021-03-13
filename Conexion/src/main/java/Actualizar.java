464c55978c6393d8e252dc430c00a4322e41f4e4
america <america.aguila1801@uppuebla.edu.mx>
2021-03-13 00:21:07 -0600

    Conexion

--- Conexion/src/main/java/Actualizar.java
+++ Conexion/src/main/java/Actualizar.java
@@ -23,25 +23,25 @@ public class Actualizar {
             //Crear conexion con base de datos
             Connection conexion=DriverManager.getConnection(url,"root","");
             //Crear una declaracion de como se va a trabajar en la clase de datos
-             //String SQL="UPDATE  usuarios SET id=?,nombre=?,contraseña=?,status=? WHERE id=?";
-             String SQL = "UPDATE usuarios SET id=? WHERE id=?";
+             String SQL="UPDATE  usuarios SET nombre = ?, contraseña = ?, status = ? WHERE id=?";
+             //String SQL = "UPDATE usuarios SET id=? WHERE id=?";
              PreparedStatement declaracion=conexion.prepareStatement(SQL);
              //Insertar datos
             System.out.println("Escribe el ID del usuario: ");
             int id=scanner.nextInt();
-            System.out.println("Escribe el nuevo ID: ");
-            int id2=scanner.nextInt();
-            /*System.out.println("Escribe el nuevo nombre del usuario:");
+            /*System.out.println("Escribe el nuevo ID: ");
+            int id2=scanner.nextInt();*/
+            scanner.nextLine();
+            System.out.println("Escribe el nuevo nombre del usuario:");
             String nombre = scanner.nextLine();
             System.out.println("Escribe la nueva contraseña: ");
             String contra = scanner.nextLine();
             System.out.println("Escribe el nuevo estatus del usuario");
-            int status= scanner.nextInt();*/
-            declaracion.setInt(1, id2);
-            declaracion.setInt(2, id);
-            /*declaracion.setString(3, nombre);
-            declaracion.setString(4, contra);
-            declaracion.setInt(5, status);*/
+            int status= scanner.nextInt();
+            declaracion.setString(1, nombre);
+            declaracion.setString(2, contra);
+            declaracion.setInt(3, status);
+            declaracion.setInt(4, id);
             //Cerrar toda conexion con la base de datos
             declaracion.executeUpdate();
             declaracion.close();
