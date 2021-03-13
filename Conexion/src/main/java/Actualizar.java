cca3471442899e800276801b93f236816129db18
america <america.aguila1801@uppuebla.edu.mx>
2021-03-12 22:51:29 -0600

    Conexion

--- Conexion/src/main/java/Actualizar.java
+++ Conexion/src/main/java/Actualizar.java
@@ -20,58 +20,34 @@ public class Actualizar {
         Scanner scanner=new Scanner(System.in);
         String url="jdbc:mysql://localhost:3306/usuarios?zeroDateTimeBehavior=CONVERT_TO_NULL";
         try{
+            //Crear conexion con base de datos
             Connection conexion=DriverManager.getConnection(url,"root","");
-             String SQL="UPDATE  usuarios SET id=?,nombre=?,contraseña=?,status=? WHERE id=?";
-             //String SQL = "UPDATE usuario SET id=?"+ "WHERE id=?";
+            //Crear una declaracion de como se va a trabajar en la clase de datos
+             //String SQL="UPDATE  usuarios SET id=?,nombre=?,contraseña=?,status=? WHERE id=?";
+             String SQL = "UPDATE usuarios SET id=? WHERE id=?";
              PreparedStatement declaracion=conexion.prepareStatement(SQL);
+             //Insertar datos
             System.out.println("Escribe el ID del usuario: ");
             int id=scanner.nextInt();
             System.out.println("Escribe el nuevo ID: ");
             int id2=scanner.nextInt();
-            System.out.println("Escribe el nuevo nombre del usuario:");
+            /*System.out.println("Escribe el nuevo nombre del usuario:");
             String nombre = scanner.nextLine();
             System.out.println("Escribe la nueva contraseña: ");
             String contra = scanner.nextLine();
             System.out.println("Escribe el nuevo estatus del usuario");
-            int status= scanner.nextInt();
+            int status= scanner.nextInt();*/
             declaracion.setInt(1, id2);
             declaracion.setInt(2, id);
-            declaracion.setString(3, nombre);
+            /*declaracion.setString(3, nombre);
             declaracion.setString(4, contra);
-            declaracion.setInt(5, status);
+            declaracion.setInt(5, status);*/
+            //Cerrar toda conexion con la base de datos
             declaracion.executeUpdate();
             declaracion.close();
             conexion.close();
             }catch(SQLException ex){
             ex.printStackTrace(System.out);
         }
-           /*try{
-            //Crear conexion con base de datos
-            Connection conexion = DriverManager.getConnection(url,"root","");
-            //Crear una declaracion de como se va a trabajar en la clase de datos
-            
-            String SQL = "UPDATE usuario SET nombre=?"+ "WHERE id=?";
-            PreparedStatement declaracion = conexion.prepareStatement(SQL); 
-            
-            //Insertar datos
-            Scanner scan = new Scanner(System.in);
-            
-            System.out.println("Ingrese ID de usuarior");
-            int id2 = scanner.nextInt();
-            System.out.println("Ingrese el nuevo nombre");
-            String nombre = scanner.nextLine();
-            
-            declaracion.setString(1, id2);
-            declaracion.setString(2, nombre);
-            
-            declaracion.executeUpdate();
-            
-            //Cerrar toda conexion con la base de datos
-            declaracion.close();
-            conexion.close();
-            
-        }catch(SQLException ex){
-            ex.printStackTrace(System.out);
-        }  */
     } 
 }
